266e5c0d564741a3f120e9ec743068626b587e14
dmitry <dmitry@dmitry.komarov>
2017-10-28 15:55:46 +0300

    Initial commit

---
+++ src/main/java/io/noop/ooparse/Parsing.java
@@ -0,0 +1,4 @@
+package io.noop.ooparse;
+
+public interface Parsing {
+}
---
+++ src/test/java/io/noop/ooparse/ParsingTest.java
@@ -0,0 +1,12 @@
+package io.noop.ooparse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParsingTest {
+    @Test
+    void testStub() {
+        assertTrue(true);
+    }
+}
